76b4c83fa30c6d812563aa2345bf77458bd6fb67
dinu.thomas@huawei.com <dinu.thomas@huawei.com>
2021-05-09 14:40:40 +0530

    Added the aggregare queries, 	new file:   medicine/aggregate.sql 	deleted:    medicine/medicine.mwb 	new file:   presentation/datadesign.png 	new file:   presentation/medicine.mwb 	modified:   presentation/medicine.pptx 	new file:   tools/data_generation_tools.ipynb 	renamed:    presentation/presentation_templates.pptx -> tools/presentation_templates.pptx

---
+++ medicine/aggregate.sql
@@ -0,0 +1,17 @@
+/**
+* The table designed for medicine project. 
+* 
+* @Auther: MTechProject Team
+* Dinu Thomas-09/05/2021- added Prescription column, medicine set into Prescription table
+* TODO , pls insert the data according to the design and 
+* add the change details as per the format<Updater>-<date>-<Update Details>
+* pls use the same comment in the commit message
+*/
+
+use medicine;
+set sql_safe_updates=0;
+
+show tables;
+
+select * from department limit 10;
+ 
